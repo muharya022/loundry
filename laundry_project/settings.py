cb1aa2f02cf3adf49ace4e16ec5276f5a5d6f135
muharya022 <muhammadaryaalfajar@gmail.com>
2026-06-19 18:44:57 +0700

    push

--- laundry_project/settings.py
+++ laundry_project/settings.py
@@ -102,30 +102,16 @@ WSGI_APPLICATION = 'laundry_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Database configuration: prefer environment-provided MySQL, fallback to SQLite for dev.
-DB_ENGINE = os.getenv('DB_ENGINE', 'django.db.backends.sqlite3')
-
-if DB_ENGINE == 'django.db.backends.mysql':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME', 'laundry_db'),
-            'USER': os.getenv('DB_USER', ''),
-            'PASSWORD': os.getenv('DB_PASSWORD', ''),
-            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-            'PORT': os.getenv('DB_PORT', '3306'),
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            },
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'laundry_db',
+        'USER': 'laundry_user',
+        'PASSWORD': 'password_mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
